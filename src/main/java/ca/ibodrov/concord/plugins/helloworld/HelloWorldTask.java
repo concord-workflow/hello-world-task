e9d894d3fe37bdd8ef8cd6e19cde4849b2d815c3
Ivan Bodrov <ibodrov@gmail.com>
2021-02-09 15:56:07 -0500

    update for the concord-v2 runtime

--- src/main/java/ca/ibodrov/concord/plugins/helloworld/HelloWorldTask.java
+++ src/main/java/ca/ibodrov/concord/plugins/helloworld/HelloWorldTask.java
@@ -1,19 +1,24 @@
 package ca.ibodrov.concord.plugins.helloworld;
 
-import com.walmartlabs.concord.sdk.Context;
-import com.walmartlabs.concord.sdk.Task;
+import com.walmartlabs.concord.runtime.v2.sdk.Task;
+import com.walmartlabs.concord.runtime.v2.sdk.TaskResult;
+import com.walmartlabs.concord.runtime.v2.sdk.Variables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Named;
 
 @Named("helloWorld")
-public class HelloWorldTask implements Task {
-
+public class HelloWorldTask
+        implements Task
+{
     private final Logger log = LoggerFactory.getLogger(HelloWorldTask.class);
 
     @Override
-    public void execute(Context ctx) throws Exception {
-        log.info("Hello, {}!", ctx.getVariable("name"));
+    public TaskResult execute(Variables input)
+            throws Exception
+    {
+        log.info("Hello, {}!", input.assertString("name"));
+        return TaskResult.success();
     }
 }
